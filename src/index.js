7e26b6d96484139777684eb3e2e74dc1de975bda
madinarsp <madinarsp20@gmail.com>
2021-08-25 13:23:35 +0600

    feat: implement check brackets sequence function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrOpenBrackets = [],
+  arrCloseBrackets = [],
+  arrIdenticals = [],
+  stack = [];
+  for(let arrElems of bracketsConfig) {
+    if(arrElems[0] === arrElems[1]) {
+      arrIdenticals.push(arrElems[0]);
+    }
+    else {
+      arrOpenBrackets.push(arrElems[0]);
+      arrCloseBrackets.push(arrElems[1]);
+    }
+  }
+  for(let i = 0; i < str.length; i++) {
+    if(arrIdenticals.includes(str[i])) {
+      if(stack.length === 0 || stack[stack.length - 1] != str[i]) {
+        stack.push(str[i]);
+        continue;
+      }
+      else {
+        stack.pop();
+        continue;
+      }
+    }
+    else if(arrOpenBrackets.includes(str[i])) {
+      stack.push(str[i]);
+    }
+    else if(arrCloseBrackets.includes(str[i]) && stack.length != 0) {
+      if ( arrOpenBrackets.indexOf(stack[stack.length - 1]) 
+        === arrCloseBrackets.indexOf(str[i]) ) {
+          stack.pop();
+          continue;
+        }
+      else return false;
+    }
+    else return false;
+  }
+  if(stack.length === 0) return true;
+  else return false;
 }
